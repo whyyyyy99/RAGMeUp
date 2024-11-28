f47ba4dfa617fc33b3f42221cba0e021113c46e4
whyyyyy99 <165992947+whyyyyy99@users.noreply.github.com>
2024-11-28 18:12:23 +0100

    Add files via upload

--- server/text2_sql.py
+++ server/text2_sql.py
@@ -16,22 +16,21 @@ class TextToSQL:
         self.logger.info(f"Translating user query: {user_query}")
         input_text = f"translate English to SQL: {user_query}"
         input_ids = self.tokenizer.encode(input_text, return_tensors="pt")
-        try:
-            outputs = self.model.generate(input_ids, max_length=128, num_beams=4, early_stopping=True)
-            sql_query = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-            self.logger.info(f"Generated SQL query: {sql_query}")  # Log the generated SQL query
-            return sql_query
-        except Exception as e:
-            self.logger.error(f"Error during translation: {str(e)}")
-            return {"error": f"Error during translation: {str(e)}"}
+        outputs = self.model.generate(input_ids, max_length=128, num_beams=4, early_stopping=True)
+        sql_query = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        print(f"Generated SQL query: {sql_query}")
+        return sql_query
 
     def execute(self, sql_query):
         try:
+            print(f"Executing SQL query: {sql_query}")
             connection = psycopg2.connect(self.db_uri)
             cursor = connection.cursor()
             cursor.execute(sql_query)
             results = cursor.fetchall()
             connection.close()
+            print(f"SQL query executed successfully, results: {results}")
             return results
         except Exception as e:
+            print(f"Error executing SQL query: {e}")
             return {"error": str(e)}
